d7097ecd9a2d78b04b5884976992eccc3e5fdd88
Promodh <160533725+Promodh-coder@users.noreply.github.com>
2024-02-25 23:40:14 +0530

    Time: 0 ms (100.00%) | Memory: 7.1 MB (50.10%) - LeetSync

---
+++ 1610-xor-operation-in-an-array/xor-operation-in-an-array.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int xorOperation(int n, int start) {
+        int nums[n],res=0;
+        for(int i = 0; i < n; i++){
+            nums[i] = start + 2 * i;
+            res^=nums[i];
+        }
+        return res;
+    }
+};

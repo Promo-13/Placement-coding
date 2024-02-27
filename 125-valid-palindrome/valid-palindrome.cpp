89918c0cf54302481bf3de7f2d47b0401e31b9c7
Promodh <160533725+Promodh-coder@users.noreply.github.com>
2024-02-27 22:40:29 +0530

    Time: 8 ms (37.41%) | Memory: 8.6 MB (40.84%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -4,8 +4,12 @@ public:
         int start=0;
         int end=s.size()-1;
         while(start<end){
-            if(!isalnum(s[start])) {start ++; continue;}
-            if(!isalnum(s[end])){end--; continue;}
+            if(!isalnum(s[start])) {
+                start ++;
+                continue;}
+            if(!isalnum(s[end])){
+                end--;
+                continue;}
             if(tolower(s[start]) != tolower(s[end])) return false;
             start++;
             end--;

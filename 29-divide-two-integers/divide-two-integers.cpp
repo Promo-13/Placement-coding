5d796b406866092fba74e8a43fd13ecb777db2e7
Promodh <160533725+Promodh-coder@users.noreply.github.com>
2024-03-02 22:33:15 +0530

    Time: 5 ms (27.42%) | Memory: 7.7 MB (10.89%) - LeetSync

--- 29-divide-two-integers/divide-two-integers.cpp
+++ 29-divide-two-integers/divide-two-integers.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     int divide(int dividend, int divisor) {
         if(dividend == INT_MIN && divisor == -1) return INT_MAX;
+        if(dividend == INT_MIN && divisor == 1) return INT_MIN;
         long long int res = dividend / divisor;
         if (res < INT_MIN){
             return INT_MIN;

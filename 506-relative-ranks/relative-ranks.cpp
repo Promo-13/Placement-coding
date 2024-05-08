3be93a828518ac14e28bfeee5c83164ed081c8be
Promodh <160533725+Promo-13@users.noreply.github.com>
2024-05-08 20:00:23 +0530

    Time: 11 ms (46.97%) | Memory: 16.2 MB (23.51%) - LeetSync

---
+++ 506-relative-ranks/relative-ranks.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<string> findRelativeRanks(vector<int>& score) {
+
+        vector<int> sortedScore = score;
+        sort(sortedScore.begin(), sortedScore.end(), greater<int>());
+        vector<string> medals = {"Gold Medal", "Silver Medal", "Bronze Medal"};
+        unordered_map<int,string> rankMapping;
+
+        for(int i = 0; i < sortedScore.size(); i++){
+            if(i < 3){
+                rankMapping[sortedScore[i]] = medals[i];
+            }else{
+                rankMapping[sortedScore[i]] = to_string(i+1);
+            }
+        }
+
+        vector<string> res;
+        for(int s: score){
+            res.push_back(rankMapping[s]);
+        }
+        
+        return res;
+    }
+};

c446bf2e1850b655f961b836dac6d56f3bae12f8
Promodh <160533725+Promo-13@users.noreply.github.com>
2024-05-20 19:32:30 +0530

    Create sum-of-all-subset-xor-totals.cpp

---
+++ 1863-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.cpp
@@ -0,0 +1,15 @@
+class Solution {
+private:
+    int dfs(vector<int>& nums, int index, int currXor){
+        if(index == nums.size()){
+            return currXor;
+        }
+        int include = dfs(nums, index+1, currXor ^ nums[index]);
+        int exclude = dfs(nums, index+1, currXor);
+        return include + exclude;
+    }
+public:
+    int subsetXORSum(vector<int>& nums) {
+        return dfs(nums, 0, 0);
+    }
+};

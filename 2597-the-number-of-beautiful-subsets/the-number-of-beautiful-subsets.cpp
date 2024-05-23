aa5d6752eceb617da7fc16711c2c37e58d61d18f
Promodh <160533725+Promo-13@users.noreply.github.com>
2024-05-23 12:27:08 +0530

    Create the-number-of-beautiful-subsets.cpp

---
+++ 2597-the-number-of-beautiful-subsets/the-number-of-beautiful-subsets.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int beautifulSubsets(vector<int>& nums, int k) {
+        ct = 0;
+        explore(nums, k, 0);
+        return ct-1;
+    }
+
+private: 
+    int ct;
+    unordered_map<int,int> visited;
+    void explore(vector<int>& nums, int k, int index){
+        if(index == nums.size()){
+            ct++;
+            return;
+        }
+        int num = nums[index];
+
+        if(visited.find(num-k) == visited.end() && 
+        visited.find(num+k) == visited.end()){
+            visited[num]++;
+            explore(nums, k, index+1);
+            visited[num]--;
+            if(visited[num]==0){
+                visited.erase(num);
+            }
+        }
+        explore(nums, k, index + 1);
+    }
+
+};
